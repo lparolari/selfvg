fe4ea1772f65927ea98ba01df12a97e16302afb7
lparolari <luca.parolari23@gmail.com>
2023-02-09 21:56:00 +0100

    chore: debug logging

--- weakvg/repo.py
+++ weakvg/repo.py
@@ -8,7 +8,7 @@ import numpy as np
 
 class ImagesSizeRepository:
     def __init__(self, images_size_path) -> None:
-        logging.info(f"Loading images size...")
+        logging.debug(f"Loading images size at {images_size_path}...")
         with open(images_size_path, "r") as f:
             self.images_size = json.load(f)
 
@@ -21,7 +21,7 @@ class ImagesSizeRepository:
 
 class ObjectsDetectionRepository:
     def __init__(self, objects_detection_path) -> None:
-        logging.info(f"Loading objects detection...")
+        logging.debug(f"Loading objects detection at {objects_detection_path}...")
         with open(objects_detection_path, "r") as f:
             self.objects_detection = json.load(f)
 
@@ -37,12 +37,12 @@ class ObjectsDetectionRepository:
 
 class ObjectsFeatureRepository:
     def __init__(self, id2idx_path, objects_feature_path) -> None:
-        logging.info(f"Loading objects feature img2id...")
+        logging.debug(f"Loading imgid2idx at {id2idx_path}...")
 
         with open(id2idx_path, "rb") as f:
             self.id2idx = pickle.load(f)
 
-        logging.info(f"Loading objects feature h5...")
+        logging.debug(f"Loading objects feature at {objects_feature_path}...")
 
         with h5py.File(objects_feature_path, "r") as hf:
             # print(hf.keys()) 	#<KeysViewHDF5 ['bboxes', 'features', 'pos_bboxes']>

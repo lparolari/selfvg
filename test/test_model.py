b28ac58c508fea71e1140fa1f3acb92f65ee27ef
lparolari <luca.parolari23@gmail.com>
2023-02-12 00:36:32 +0100

    test: visual branch

--- test/test_model.py
+++ test/test_model.py
@@ -1,10 +1,9 @@
 import unittest
 
 import torch
-import torch.nn as nn
 from torch.nn.functional import cosine_similarity
 
-from weakvg.model import TextualBranch, WordEmbedding
+from weakvg.model import TextualBranch, WordEmbedding, VisualBranch, ConceptBranch
 from weakvg.wordvec import get_wordvec
 
 
@@ -49,3 +48,43 @@ class TestTextualBranch(unittest.TestCase):
 
         self.assertEqual(output.shape, (1, 2, 300))
         self.assertEqual(mask.shape, (1, 2, 1))
+
+
+class TestVisualBranch(unittest.TestCase):
+    wordvec, vocab = get_wordvec()
+    we = WordEmbedding(wordvec, vocab)
+
+    def test_spatial(self):
+        proposals = torch.tensor([[[20, 20, 80, 100]]])  # [b, p, 4]
+        image_w = torch.tensor([500])
+        image_h = torch.tensor([335])
+
+        visual_branch = VisualBranch(word_embedding=self.we)
+
+        spat = visual_branch.spatial(
+            {
+                "proposals": proposals,
+                "image_w": image_w,
+                "image_h": image_h,
+            }
+        )
+
+        self.assertEqual(spat.shape, (1, 1, 5))
+
+        self.assertAlmostEqual(spat[0, 0, 0].item(), 20 / 500)
+        self.assertAlmostEqual(spat[0, 0, 1].item(), 20 / 335)
+        self.assertAlmostEqual(spat[0, 0, 2].item(), 80 / 500)
+        self.assertAlmostEqual(spat[0, 0, 3].item(), 100 / 335)
+        self.assertAlmostEqual(
+            spat[0, 0, 4].item(), ((80 - 20) * (100 - 20)) / (500 * 335)
+        )
+
+    def test_project(self):
+        proposals_feat = torch.rand(2, 6, 3)  # [b, p, v]
+        spat = torch.rand(2, 6, 5).mul(100).long()  # [b, p, 5]
+
+        visual_branch = VisualBranch(word_embedding=self.we)
+
+        proj = visual_branch.project(proposals_feat, spat)
+
+        self.assertEqual(proj.shape, (2, 6, 8))

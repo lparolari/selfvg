fix: nan gradient due to division by zero

weakvg/loss.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-from weakvg.masking import get_queries_count
+from weakvg.masking import get_queries_count, get_queries_mask
 
 
 class Loss(nn.Module):
@@ -20,18 +20,15 @@ class Loss(nn.Module):
         queries = x["queries"]  # [b, q, w]
         scores = x["scores"]  # [b, q, b, p]
 
-        queries_mask = (queries != 0) # [b, q, w]
-        n_words = queries_mask.sum(-1)  # [b, q]
-        is_query = n_words > 0  # [b, q]
-        n_queries = is_query.sum(-1).unsqueeze(-1)  # [b, 1]
+        _, is_query = get_queries_mask(queries)  # [b, q, w], [b, q]
+        _, n_queries = get_queries_count(queries)  # [b, q], [b]
+        n_queries = n_queries.unsqueeze(-1)  # [b, 1]
         has_query = is_query.any(-1).unsqueeze(-1)  # [b, 1]
 
-        queries_e = self.we(queries, queries_mask)  # [b, q, w, d]
-
-        x = {**x, "queries_e": queries_e}
-
         scores, _ = scores.max(-1)  # [b, q, b]
-        scores = scores.sum(-2) / n_queries  # [b, b]
+
+        # scores = scores * is_query.unsqueeze(-1)  # TODO: is this required?
+        scores = scores.sum(-2) / n_queries.clamp(1)  # [b, b]
         scores = scores.masked_fill(~has_query, 0)
 
         pos = self.select_pos(x)  # [b, b]
@@ -56,15 +53,12 @@ class Loss(nn.Module):
         :return: A tensor of shape `[b, b]` with negative mask
         """
         queries = x["queries"]  # [b, q, w]
-        queries_e = x["queries_e"]  # [b, q, w, d]
 
         if self.neg_selection == "textual_sim_max":
-            # select the most similar example
+            # select the most similar example according to query similarity
 
-            n_words, n_queries = get_queries_count(queries)  # [b, q], [b]
-
-            sim = self.textual_sim(queries_e, n_words, n_queries)  # [b, b]
-            sim = (sim + 1) / 2  # [b, b]
+            sim = self.textual_sim(queries)  # [b, b]
+            sim = (sim + 1) / 2  # [b, b]  - translate and scale to range [0, 1]
 
             # filter positive examples, which have similarity 1
             sim = sim.masked_fill(pos, 0)  # [b, b]
@@ -80,20 +74,27 @@ class Loss(nn.Module):
         # default to random selection
         return pos.roll(-1, dims=0)  # [b, b]
 
-    def textual_sim(self, queries_e, n_words, n_queries):
+    def textual_sim(self, queries):
         """
         Returns a tensor of shape `[b, b]` with the averaged similarity between examples.
         """
+        is_word, _ = get_queries_mask(queries)  # [b, q, w], [b, q]
+        n_words, n_queries = get_queries_count(queries)  # [b, q], [b]
+
+        queries_e = self.we(queries, is_word)  # [b, q, w, d]
+
         b = queries_e.shape[0]
 
-        # averaged query representation over words
-        query_repr = queries_e.sum(dim=-2) / n_words.unsqueeze(-1)  # [b, q, d]
+        query_repr = queries_e.masked_fill(~is_word.unsqueeze(-1), 0)
+
+        # compute the averaged query representation over words
+        query_repr = queries_e.sum(dim=-2) / n_words.clamp(1).unsqueeze(-1)  # [b, q, d]  - clamp is required to avoid division by 0
         query_repr = query_repr.masked_fill(
             (n_words == 0).unsqueeze(-1), value=0
         )  # [b, q, d]
 
         # averaged query representation over phrases
-        query_repr = query_repr.sum(dim=-2) / n_queries.unsqueeze(-1)  # [b, d]
+        query_repr = query_repr.sum(dim=-2) / n_queries.clamp(1).unsqueeze(-1)  # [b, d]
         query_repr = query_repr.masked_fill(
             (n_queries == 0).unsqueeze(-1), value=0
         )  # [b, d]

weakvg/model.py
@@ -29,9 +29,8 @@ class WeakvgModel(pl.LightningModule):
         super().__init__()
         self.use_relations = use_relations
         self.lr = lr
-        we = WordEmbedding(
-            wordvec, freeze=True
-        )  # TODO: temporarily freezed, revert back to False
+        # TODO: temporarily freezed, revert back to False
+        we = WordEmbedding(wordvec, freeze=True)
         we_freezed = WordEmbedding(wordvec, freeze=True)
         self.concept_branch = ConceptBranch(word_embedding=we_freezed)
         self.visual_branch = VisualBranch(word_embedding=we)
@@ -296,30 +295,44 @@ class WordEmbedding(nn.Module):
 
         if issubclass(type(wordvec), BertModel):
             self.factory = "bert"
-            self.backbone = wordvec
+            self.bert = wordvec
+            self.lin = nn.Linear(768, 300)
 
-            for param in self.backbone.parameters():
+            for param in self.bert.parameters():
                 param.requires_grad = not freeze
+
+            nn.init.xavier_uniform_(self.lin.weight)
+            nn.init.zeros_(self.lin.bias)
 
     def forward(self, x, *args, **kwargs):
         if self.factory == "bert":
-            kwargs = {"return_dict": False} | kwargs
             mask = args[0]
+            return self.forward_bert(x, mask, **kwargs)
 
-            sh = x.shape
+        if self.factory == "vectors":
+            return self.emb(x)
 
-            input_ids = x.reshape(-1, sh[-1])
-            attention_mask = mask.reshape(-1, sh[-1]).long()
+        raise NotImplementedError(
+            f"WordEmbedding does not implement '{self.factory}' factory"
+        )
 
-            out, _ = self.backbone(input_ids, attention_mask, **kwargs)
+    def forward_bert(self, x, mask, **kwargs):
+        kwargs = {"return_dict": False} | kwargs
 
-            out = out.reshape(*sh, -1)
+        sh = x.shape
 
-            out = out.masked_fill(~mask.unsqueeze(-1), 0)
+        input_ids = x.reshape(-1, sh[-1])
+        attention_mask = mask.reshape(-1, sh[-1]).long()
 
-            return out
+        out, _ = self.bert(input_ids, attention_mask, **kwargs)
 
-        return self.emb(x)
+        out = out.reshape(*sh, -1)
+
+        out = out.masked_fill(~mask.unsqueeze(-1), 0)
+        out = self.lin(out)
+        out = out.masked_fill(~mask.unsqueeze(-1), 0)
+
+        return out
 
 
 class ConceptBranch(nn.Module):
@@ -332,19 +345,22 @@ class ConceptBranch(nn.Module):
         heads = x["heads"]  # [b, q, h]
         labels = x["labels"]  # [b, p]
 
-        heads_mask = heads != 0
+        heads_mask = heads != 0  # [b, q, h]
         n_heads = heads_mask.sum(-1).unsqueeze(-1)  # [b, q, 1]
 
         label_mask = labels != 0
 
         heads_e = self.we(heads, heads_mask)  # [b, q, h, d]
-        heads_e = heads_e.sum(-2) / n_heads  # [b, q, d]
+        heads_e = heads_e.masked_fill(~heads_mask.unsqueeze(-1), 0.)
+        heads_e = heads_e.sum(-2) / n_heads.clamp(1)  # [b, q, d]  - clamp is required to avoid div by 0
         heads_e = heads_e.unsqueeze(-2).unsqueeze(-2)  # [b, q, 1, 1, d]
 
         labels_e = self.we(labels, label_mask)  # [b, p, d]
         labels_e = labels_e.unsqueeze(0).unsqueeze(0)  # [1, 1, b, p, d]
 
         scores = self.sim_fn(heads_e, labels_e)  # [b, q, b, p]
+
+        scores = scores.masked_fill(~get_concepts_mask_(x), 0)
 
         return scores
 
@@ -355,7 +371,7 @@ class VisualBranch(nn.Module):
 
         self.we = word_embedding
 
-        self.fc = nn.Linear(2048 + 5, 768)
+        self.fc = nn.Linear(2048 + 5, 300)
         self.act = nn.LeakyReLU()
 
         self._init_weights()
@@ -409,7 +425,7 @@ class TextualBranch(nn.Module):
     def __init__(self, word_embedding):
         super().__init__()
         self.we = word_embedding
-        self.lstm = nn.LSTM(768, 768)
+        self.lstm = nn.LSTM(300, 300)
 
     def forward(self, x):
         from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence